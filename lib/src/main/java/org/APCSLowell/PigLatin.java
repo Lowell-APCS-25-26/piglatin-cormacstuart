8fd14cd3cd59a4b7f708403e010f35bfc9dce559
cormacstuart <stuartcormac11@gmail.com>
2026-01-21 17:11:07 +0000

    test6

--- lib/src/main/java/org/APCSLowell/PigLatin.java
+++ lib/src/main/java/org/APCSLowell/PigLatin.java
@@ -14,7 +14,8 @@ public class PigLatin {
     public String pigLatin(String sWord) {
         //precondition: sWord is a valid String of length greater than 0
         //postcondition: returns the pig latin equivalent of sWord
-        if (findFirstVowel(sWord)<=0) return sWord + "ay";
+        if (charAt(0)==q) return sWord.substring(2, sWord.length())+"quay";
+        else if (findFirstVowel(sWord)<=0) return sWord + "ay";
         else return (sWord.substring(findFirstVowel(sWord), sWord.length())+sWord.substring(0,findFirstVowel(sWord))+"ay");
     }
 }
